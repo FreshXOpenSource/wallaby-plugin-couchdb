46ce0ad41ce06f39172d2cdaffa5addded72961d
Dominik Roessler <dominik@freshx.de>
2013-01-17 14:27:39 +0100

    changed version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='wallaby-plugin-couchdb',
-      version='0.1.10',
+      version='0.1.11',
       url='https://github.com/FreshXOpenSource/wallaby-plugin-couchdb',
       author='FreshX GbR',
       author_email='wallaby@freshx.de',
